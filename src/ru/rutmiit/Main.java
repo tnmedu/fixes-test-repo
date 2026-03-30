6202b1905024a20929cffec8f180842d0f46bc65
Nikita <net058230@gmail.com>
2026-03-30 14:08:24 +0300

    add new function

--- src/ru/rutmiit/Main.java
+++ src/ru/rutmiit/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world!");
     }
+
+    public static void newFunction() {
+        System.out.println("hello!");
+    }
 }
